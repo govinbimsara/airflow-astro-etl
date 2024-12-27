e1474abe4e94a07237a2f91a1585fe85e311c716
Govin <govinbimsara@gmail.com>
2024-12-27 12:19:01 +0000

    Code syntax correction

--- dags/etl.py
+++ dags/etl.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.decorators import task
-from airflow.decorators.postgres.hooks.postgres import PostgresHooks
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.dates import days_ago
 import json
 
@@ -21,13 +21,13 @@ with DAG(
 
         #SQL to create table
         create_table_query = """
-            CRREATE TABLE IF NOT EXISTS apod_data (
+            CREATE TABLE IF NOT EXISTS apod_data (
                 id SERIAL PRIMARY KEY,
                 title VARCHAR(225),
                 explanation TEXT,
                 url TEXT,
                 data DATE,
-                media_type VARCAHR(50)
+                media_type VARCHAR(50)
             );
         
         """
@@ -37,7 +37,7 @@ with DAG(
     ##Extract data from the NASA api
     extract_epod = SimpleHttpOperator(
         task_id = 'extract_apod',
-        htttp_conn_id = 'nasa_api', #connection id that has been defined in airflow for NASA api
+        http_conn_id = 'nasa_api', #connection id that has been defined in airflow for NASA api
         endpoint = 'planetary/apod',
         method = 'GET',
         data = {"api_key":"{{ conn.nasa_api.extra_dejson.api_key}}"},
