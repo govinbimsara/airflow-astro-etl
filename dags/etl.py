082d40ee0a2326e250e8668675446c2fdf372d3a
Govin <govinbimsara@gmail.com>
2024-12-27 06:49:09 +0000

    Table creation query in ETL and docker-compose setup for postgres through docker

--- dags/etl.py
+++ dags/etl.py
@@ -11,4 +11,25 @@ with DAG(
     schedule_interval = '@daily',
     catchup = False
 ) as dag:
-    pass
+    
+    #Create table to load data from api
+
+    @task
+    def create_table():
+        #initialize postgreshook
+        postgres_hook = PostgresHooks(postgres_conn_id = 'my_postgres_connection')
+
+        #SQL to create table
+        create_table_query = """
+            CRREATE TABLE IF NOT EXISTS apod_data (
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(225),
+                explanation TEXT,
+                url TEXT,
+                data DATE,
+                media_type VARCAHR(50)
+            );
+        
+        """
+        ##execute table creation query
+        postgres_hook.run(create_table_query)
